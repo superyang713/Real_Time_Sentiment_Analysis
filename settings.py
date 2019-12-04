77b7dc2e21e13e1cb80288eece8d5608c8d7d585
Yang Dai <yang.dai2020@gmail.com>
2019-12-04 02:29:51 -0500

    Use database connection object to store tweets from streaming

--- settings.py
+++ settings.py
@@ -1,2 +1,5 @@
 tracking = ["apple"]
 languages = ["en"]
+
+table_name = "test"
+table_attributes = "id_str VARCHAR(255), text VARCHAR(255)"
--- stream.py
+++ stream.py
@@ -1,5 +1,8 @@
-import credentials
 import tweepy
+
+import credentials
+import settings
+from database import Connection
 
 
 class MyStreamListener(tweepy.StreamListener):
@@ -7,7 +10,6 @@ class MyStreamListener(tweepy.StreamListener):
     Twitter stream object.
     It contains all the logics about how to stream tweets.
     """
-
     def on_status(self, status):
         """
         Storing streaming tweets in the SQL database.
@@ -16,7 +18,18 @@ class MyStreamListener(tweepy.StreamListener):
         # Exclude all retweets.
         if status.retweeted or 'RT @' in status.text:
             return True
-        print(status.text)
+
+        id_str = status.id_str
+        text = status.text
+        print(text)
+
+        insert_sql = "INSERT INTO {} (id_str, text) VALUES (%s, %s) ".format(settings.table_name)
+
+        val = (id_str, text)
+
+        with Connection() as db:
+            db.create_table_if_not_existed(settings.table_name)
+            db.execute(insert_sql, val)
 
     def on_error(self, status_code):
         """
